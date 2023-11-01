e722e1822b337112385f52363fdb0890392317f6
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-11-01 20:05:09 +0100

    Added the DB Connection URL.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'config/config';
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${config.database.DB_USERNAME}:${config.database.DB_PASSWORD}@customers.jlfz2cl.mongodb.net/?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
